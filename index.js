3223475fb73afefda0381e2c6697655c4d4fa61c
Sam Fung <sam.fung@gmail.com>
2018-11-23 11:25:36 -0800

    Add missing index.js and remove unused npm packages

---
+++ index.js
@@ -0,0 +1,7 @@
+const sequelizeStore = require('./lib/sequelize-store.js');
+const PasswordlessToken = require('./lib/passwordless-token');
+
+module.exports = {
+  sequelizeStore,
+  PasswordlessToken,
+};
