9e179a05bfe09c4fb7f8bef6a833be3896c63317
Sam Fung <sam.fung@gmail.com>
2018-11-23 11:00:28 -0800

    Add implementation and tests

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # passwordless-sequelizestore
 TokenStore for Passwordless using Sequelize
+
+```
+CREATE TABLE IF NOT EXISTS `passwordless_tokens` (
+  `uid` VARCHAR(255) NOT NULL PRIMARY KEY,
+  `token` VARCHAR(255) NOT NULL,
+  `ttl` INTEGER NOT NULL,
+  `origin_url` VARCHAR(255)
+);
+
+CREATE UNIQUE INDEX `passwordless_tokens_token` ON `passwordless_tokens` (`token`);
+```
---
+++ test/sequelize-store.spec.js
@@ -0,0 +1,30 @@
+const standardTests = require('passwordless-tokenstore-test');
+const Sequelize = require('sequelize');
+const SequelizeStore = require('../lib/sequelize-store');
+const expect = require('chai').expect;
+const path = require('path');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  logging: false,
+});
+sequelize['import'](path.join(__dirname, '..', 'lib', 'passwordless-token'));
+
+function TokenStoreFactory() {
+  return new SequelizeStore(sequelize);
+}
+
+function beforeEachTest(done) {
+  // Drop all tables and create again.
+  return sequelize.sync({ force: true }).then(() => {
+    done();
+  });
+}
+
+function afterEachTest(done) {
+  done();
+}
+
+// Call the test suite
+standardTests(TokenStoreFactory, beforeEachTest, afterEachTest);
